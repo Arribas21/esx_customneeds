b99a2ba3ecbac3696252ece252bfb668785bdaa7
Alvaro Arribas <alvaroarribas1994@gmail.com>
2018-05-31 00:04:23 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -4,20 +4,14 @@ version '1.0.1'
 
 server_scripts {
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
 	'locales/en.lua',
-	'locales/fr.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
 	'locales/en.lua',
-	'locales/fr.lua',
 	'config.lua',
 	'client/main.lua'
 }
